08ff04595ca69534810013cb1b3ca614c7c9b4fd
vagrant <vagrant@localhost.localdomain>
2019-05-14 16:19:49 +0900

    New branch multiple_page

--- app/controllers/course_searcher_controller.rb
+++ app/controllers/course_searcher_controller.rb
@@ -2,6 +2,9 @@ class CourseSearcherController < ApplicationController
   def top
   end
 
+  def multiple_list
+  end
+
   def error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'course_searcher/top'
 
+  get 'course_searcher/multiple_list'
+
   get 'course_searcher/error'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
