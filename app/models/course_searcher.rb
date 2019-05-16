9e9eecb4524f2d0b974ca611ea63ea5785f1edb9
vagrant <vagrant@localhost.localdomain>
2019-05-16 16:47:40 +0900

    Add a search system

--- app/models/course_searcher.rb
+++ app/models/course_searcher.rb
@@ -1,10 +1,17 @@
 class CourseSearcher < ApplicationRecord
-  def self.search(search)
+  def self.keyword_search(search)
     if search
-      where(['course_title LIKE ?', "%#{search}%"])
+      where(['concat(course_id, course_title, topic, day_length, price, level_id, category) LIKE ?', "%#{search}%"])
     else
       all
     end
   end
 
+  def self.category_search(search)
+    if search
+      where(['category LIKE ?', "%#{search}%"])
+    else
+      all
+    end
+  end
 end
