3abd6af6b3998aefa7c64f83be1a0c0a1f136266
vagrant <vagrant@localhost.localdomain>
2019-05-29 17:04:43 +0900

    login user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    puts params
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
